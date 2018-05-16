320b7a77cb226e1087323ab3ad49a7371b01fe4a
gotoney <everal@yeah.net>
2018-05-16 18:14:10 +0800

    update shiro config

--- src/main/java/com/zeecle/springboot/shiro/ShiroConfig.java
+++ src/main/java/com/zeecle/springboot/shiro/ShiroConfig.java
@@ -52,6 +52,7 @@ public class ShiroConfig {
 		Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
 		filterChainDefinitionMap.put("/login", "anon");
 		filterChainDefinitionMap.put("/external/**", "anon");
+		filterChainDefinitionMap.put("/druid/**", "anon");
 		filterChainDefinitionMap.put("/static/**", "anon");
 		filterChainDefinitionMap.put("/logout", "logout");
 		filterChainDefinitionMap.put("/**", "authc");
